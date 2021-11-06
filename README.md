a18b6e720f6a3f457d520e8c09d68d1b18228ab7
Glory-better <2870186081@qq.com>
2021-11-07 00:51:08 +0800

    2021/11/06

--- README.md
+++
@@ -1,1 +0,0 @@
-## 代码仓库:https://github.com/Glory-better/ZiQiangStudioBackstageGroupTraining/tree/master
---
+++ typeidea/urls.py
@@ -0,0 +1,37 @@
+"""typeidea URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path,re_path
+from django.contrib import admin
+
+from blog.views import (
+IndexView,CategoryView,TagView,PostDetailView,AuthorView,SearchView)
+from comment.views import CommentView
+from config.views import LinkListView
+from .custom_site import custom_site
+
+urlpatterns = [
+    re_path(r'^$',IndexView.as_view(),name='index'),
+    re_path(r'category/(?P<category_id>\d+)/$',CategoryView.as_view(),name='category-list'),
+    re_path(r'^tag/(?P<tag_id>\d+)/$',TagView.as_view(),name='tag-list'),
+    re_path(r'^post/(?P<post_id>\d+).html$',PostDetailView.as_view(),name='post-detail'),
+    re_path(r'^links/$',LinkListView.as_view(),name='links'),
+    re_path(r'^super_admin/', admin.site.urls,name='super-admin'),
+    re_path(r'^admin/',custom_site.urls,name='admin'),
+    re_path(r'^search/$',SearchView.as_view(),name='search'),
+    re_path(r'^author/(?P<owner_id>\d+)/$',AuthorView.as_view(), name='author'),
+    re_path(r'^links/$',LinkListView.as_view(),name='links'),
+    re_path(r'^comment/$',CommentView.as_view(),name='comment'),
+]
